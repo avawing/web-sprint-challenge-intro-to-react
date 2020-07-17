768515621733dd8d10f785ff2645bae3eb728bf8
Aveline Wingfield <avelinewingfield@gmail.com>
2020-07-17 14:06:49 -0400

    gender

--- starwars/src/components/Stats.js
+++ starwars/src/components/Stats.js
@@ -5,7 +5,7 @@ function Stats(props){
     const {data, fadeIn} = props
 return(
 <Fade in={fadeIn} tag="h5">
-    {data.name} is {data.gender}, {data.height}cm and has {data.mass}kg of mass. <br/>
+    {data.name} is {data.height}cm and has {data.mass}kg of mass. <br/>
     Approach with Caution.<br />
     They are considered to be armed <br />
 </Fade>
