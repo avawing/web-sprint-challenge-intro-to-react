36fc38875af73f640738b7e48e9d821395af23bd
Aveline Wingfield <avelinewingfield@gmail.com>
2020-07-17 13:02:01 -0400

    API - easy. Styling? forever T-T

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,32 @@
 // Write your Character component here
+import React from 'react';
+import {
+    Card, CardText, CardBody,
+    CardTitle, CardSubtitle, Button
+  } from 'reactstrap';
+
+function Character(props){
+    const {data} = props
+
+
+    
+    return (
+        <div>
+          <Card>
+            <CardBody>
+              <CardTitle>{data.name}</CardTitle>
+              <CardSubtitle>{data.birth_year}</CardSubtitle>
+              <CardText>
+                  gender: {data.gender}<br />
+                  hair color: {data.hair_color}<br />
+                  height: {data.height}<br />
+                  mass: {data.mass}<br />
+                  skin_color: {data.skin_color}
+                  </CardText>
+            </CardBody>
+          </Card>
+        </div>
+      );
+    };
+
+export default Character
